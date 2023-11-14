3aceb8406ae1ec9d16e701a3fa4d7b5df8e1dbfa
Neil Hutchinson <neilhutchinson.wd@gmail.com>
2023-11-14 20:09:40 +0800

    update About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,25 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <!-- <base-button /> -->
+    <!-- Introduction -->
+    <section class="mb-8 py-20 text-white text-center relative">
+      <div
+        class="absolute inset-0 w-full h-full bg-contain introduction-bg"
+        style="background-image: url(assets/img/header.png)"
+      ></div>
+      <div class="container mx-auto">
+        <div class="text-white main-header-content">
+          <h1 class="font-bold text-5xl mb-5">{{ $t('home.listen') }}</h1>
+          <h1 class="w-full md:w-8/12 mx-auto">
+            This is an about page!
+          </h1>
+        </div>
+      </div>
+
+      <img
+        class="relative block mx-auto mt-5 -mb-20 w-auto max-w-full"
+        src="/assets/img/introduction-music.png"
+      />
+    </section>
   </div>
 </template>
 
