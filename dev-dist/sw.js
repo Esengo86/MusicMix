a51fea1a90cf4e5b399c571dcf27c8ffde8550d4
Neil Hutchinson <neilhutchinson.wd@gmail.com>
2023-11-14 20:18:56 +0800

    modified:   dev-dist/sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.9k4cdhh742g"
+    "revision": "0.ga1epsm2pq8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
